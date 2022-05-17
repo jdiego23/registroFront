b0627cbfacc4cc095ea75dc92d34249316a7480d
jdiego23 <jjuandiego23@gmail.com>
2022-05-16 22:08:53 -0500

    WorkFlows

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Historial de Registro');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
-  });
+
 });
